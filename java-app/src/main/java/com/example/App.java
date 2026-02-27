d867ac249a64e4dc10ae7d6cae9ffa4809ee0624
sangbeome <sangbeomg592@gmail.com>
2026-02-27 13:17:37 +0900

    Add Java Maven project for build testing

---
+++ java-app/src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Java Maven App!");
+        System.out.println("Build test successful.");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
